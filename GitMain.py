bbdb8ec43f7c6262cca56c607f46d8adfca8e8d5
Sri Pudipeddi <skyp1729@gmail.com>
2017-09-24 15:17:32 -0700

    added documentation and made more function calls.

--- GitMain.py
+++ GitMain.py
@@ -1,8 +1,10 @@
 def talking(worda, wordb):
+    # type: (str, str) -> str
     c = worda + wordb
     return c
 
 print talking("Sunny", " sky")
-worda = raw_input("Enter a word: ")
-wordb = raw_input("Enter another word: ")
-print talking(worda, wordb)
+
+print talking(worda="Baking", wordb=" cookies")
+talking("cool", "off")
+
