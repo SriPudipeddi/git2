846ee93e08fad3de8463f47b6539e3011fcb4708
Sri Pudipeddi <skyp1729@gmail.com>
2017-09-24 15:30:13 -0700

    added new function divby2

--- GitMain.py
+++ GitMain.py
@@ -8,3 +8,11 @@ print talking("Sunnie", " sky")
 print talking(worda="Baking", wordb=" cookies")
 talking("cool", "off")
 
+def divby2(a):
+    if a%2 == 0:
+        print "%d is a multiple of 2" %a
+    else:
+        print "%d is not a multiple of 2" %a
+
+
+divby2(101)
